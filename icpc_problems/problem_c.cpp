ad27a74071a2d583eecfddf9f99125987c7a423a
Lars17S <44946904+Lars17S@users.noreply.github.com>
2019-08-14 18:14:22 -0600

    Update problem_c.cpp

--- icpc_problems/problem_c.cpp
+++ icpc_problems/problem_c.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	int n, m, left, right, pos;
 	char s;
-    unordered_map<int, int> id;
+    	unordered_map<int, int> id;
 	left = 0; 
 	right = 0;
 	cin >> n; // Set number of instructions
@@ -27,7 +27,7 @@ int main()
 			id[m] = right;
 			break;
 		case '?':
-            pos = id.at(m);
+            		pos = id.at(m);
 			cout << min(abs(left - pos), abs(right - pos)) << endl;
 			break;
 		}
