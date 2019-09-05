1810e78d787a65495e607778a0fa8c43af310ef0
Lars17S <efren.garibaldi@gmail.com>
2019-09-05 12:19:46 -0500

    marble changed

--- spoj_problems/marbles.cpp
+++ spoj_problems/marbles.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-long long int binomial_coefficient(long long int n, long long int k);
+long long int binomial_coefficient(int n, int k);
 
 int main()
 {
 	short T;
-	long long int n, k, maxDiv, minDiv;
+	int n, k;
 	cin >> T;
 	for (short i = 0; i < T; ++i) {
 		cin >> n >> k;
 		if (n == k || k == 1)
 			cout << 1 << endl;
-		else {
-			minDiv = min((k - 1), ((n - 1) - (k - 1)));
-			cout << binomial_coefficient(minDiv, n - 1) << endl;
-		}
+		else
+			cout << binomial_coefficient(min((k - 1), ((n - 1) - (k - 1))), n - 1) << endl;
 	}
 
 	return 0;
 }
 
-long long int binomial_coefficient(long long int n, long long int k) {
+long long int binomial_coefficient(int n, int k) {
 	//cout << "Num: " << (k - (n - 1)) << " | Den: " << n << endl;
 	if (n == 1)
 		return k; // k / 1
